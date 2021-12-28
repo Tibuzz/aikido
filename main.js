da2272d46cf5bfad4a5e5d61e7f5a891f358cdaa
Tibuzz <tibuzz@hotmail.com>
2021-12-28 12:35:23 -0600

    Highlight active tab

---
+++ main.js
@@ -0,0 +1,26 @@
+// Get the address of active tab 
+let activeTab = window.location.pathname;
+
+// Declare the link box to color
+let link;
+
+// Color the active tab name
+
+if (activeTab.includes('index')) {
+   link = document.getElementById('index-link');
+} else if (activeTab.includes('club')) {
+  link = document.getElementById('club-link');
+} else if (activeTab.includes('photos')) {
+  link = document.getElementById('photos-link');
+} else if (activeTab.includes('infos')) {
+  link = document.getElementById('infos-link');
+  } 
+
+  link.style.backgroundColor = 'white';
+  link.style.color = 'black';
+
+  // Uncolor it on :hover
+  
+
+
+
