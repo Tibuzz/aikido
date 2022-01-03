0e4d4ab03bf5f0713ffed2fffaa5576c06eb0138
Tibuzz <tibuzz@hotmail.com>
2022-01-02 18:07:00 -0600

    Ajout accueil

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+// Transition accueil.html
+let p1 = document.getElementById('1');
+let p2 = document.getElementById('2');
+let button = document.getElementById('button');
+
+main.onmouseover = (e) => {
+  p1.style.transitionDelay = '300ms';
+  p2.style.transitionDelay = '2s';
+  button.style.transitionDelay ='3500ms';
+  p1.style.transitionDuration = '3s';
+  p2.style.transitionDuration = '3s';
+  button.style.transitionDuration = '3s';
+  p1.style.opacity = 1;
+  p2.style.opacity = 1;
+  button.style.opacity = 1;
+  p2.style.marginBottom = '8rem';
+}
+
+console.log(enter);
+
 // Get the address of active tab 
 let activeTab = window.location.pathname;
 
